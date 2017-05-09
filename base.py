81f4dd2e7b5ed06cfccaf6411c19b02c1d6260bc
haithcockce <haithcockce@gmail.com>
2017-05-09 00:11:32 -0400

    Redirection to my sites added. Now to actually put in sites

--- base.py
+++ base.py
@@ -4,8 +4,12 @@ from flask import render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
+def index():
     return render_template('index.html')
+
+@app.route('/mysites')
+def mysites():
+    return render_template('mysites.html')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
